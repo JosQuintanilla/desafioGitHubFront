54bd8dd03dacb8600e8e48a3a60b23eabf4a1bb8
ext_waguerom <ext_waguerom@falabella.cl>
2019-06-19 10:29:41 -0400

    JQ cambios en server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/desafio-gut-hub-front'));
+app.use(express.static(__dirname + '/dist/mysterious-island-77334.herokuapp.com/'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname+ '/dist/desafio-gut-hub-front/index.html'));
+  res.sendFile(path.join(__dirname+ '/dist/mysterious-island-77334.herokuapp.com//index.html'));
 });
 console.log('aasdfasdfasdfasdfasdfa');
 // Start the app by listening on the default Heroku port
