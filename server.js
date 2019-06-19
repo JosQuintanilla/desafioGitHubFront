d81119bc415050330a4758b0c17357e1a3f109bd
ext_waguerom <ext_waguerom@falabella.cl>
2019-06-19 10:45:40 -0400

    JQ se cambia nombre en server.js

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/mysterious-island-77334.herokuapp.com/'));
+app.use(express.static(__dirname + '/dist/desafioFront/'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname+ '/dist/mysterious-island-77334.herokuapp.com//index.html'));
+  res.sendFile(path.join(__dirname+ '/dist/desafioFront/index.html'));
 });
-console.log('aasdfasdfasdfasdfasdfa');
+console.log('Server Up');
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
